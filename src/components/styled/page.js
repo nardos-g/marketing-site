026187c226ef548a890e7356889eee095d17fc86
Nardos Getachew <nardos1g3@gmail.com>
2023-01-26 21:18:36 +0300

    created a gatsby project with netlify cms

---
+++ src/components/styled/page.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Page = styled.div`
+    color: "#232129";
+    padding: 96px;
+    font-family: "-apple-system, Roboto, sans-serif, serif";
+`
+
+
+
+export default Page
